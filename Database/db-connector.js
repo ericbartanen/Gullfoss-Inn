530981a6819f82e9c10ad78e31f03bf123462d66
Eric Bartanen <eric.bartanen@gmail.com>
2023-07-20 21:58:44 -0700

    more db connection options, trying to solve sequence timeout so increasing connection time allowed

--- Database/db-connector.js
+++ Database/db-connector.js
@@ -3,8 +3,10 @@
 var mysql = require('mysql')
 
 var pool = mysql.createPool({
-    connectionLimit : 10,
-    // acquireTimeout  : 100000,
+    connectionLimit : 100,
+    acquireTimeout  : 1000000,
+    connectTimeout  : 30000,
+    debug           : false,
     host            : process.env.DB_HOST,
     user            : process.env.DB_USER,
     password        : process.env.DB_PASSWORD,
