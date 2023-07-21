cfe003bd5066da9d5d185a6b3ded86bda11a25d9
Eric Bartanen <eric.bartanen@gmail.com>
2023-07-20 21:30:50 -0700

    updated dependencies, commented out some DB connection options

--- Database/db-connector.js
+++ Database/db-connector.js
@@ -4,13 +4,13 @@ var mysql = require('mysql')
 
 var pool = mysql.createPool({
     connectionLimit : 10,
-    acquireTimeout  : 100000,
+    // acquireTimeout  : 100000,
     host            : process.env.DB_HOST,
     user            : process.env.DB_USER,
     password        : process.env.DB_PASSWORD,
     database        : process.env.DB, 
     port            : process.env.PORT,
-    ssl             : process.env.SSL,
+    // ssl             : process.env.SSL,
 })
 
 // Export 
